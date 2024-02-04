5dc9405c73e8acc88741f520fb30ab767578aa72
Sowrabhagowda <142792587+Sowrabhagowda@users.noreply.github.com>
2024-02-05 00:33:53 +0530

    Add files via upload

---
+++ project 0.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ##  1 User Input
+
+# In[2]:
+
+
+a = 12
+b = 15
+
+
+# ## 2 Arithmetic Operations
+
+# ##  3 and Output display
+
+# In[5]:
+
+
+print('a' '+' 'b' '=',a+b)
+print('a' '-' 'b' '=',a-b)
+print('a' '*' 'b' '=',a*b)
+print('a' '/' 'b' '=',a/b)
+print('a' '%' 'b' '=',a%b)
+
+
+# ##  4 Type casting
+
+# In[8]:
+
+
+int(-3)
+int(0.8)
+
+
+# In[9]:
+
+
+float(12)
+
+
+# In[10]:
+
+
+float(-3)
+
+
+# In[11]:
+
+
+float(27)
+
+
+# In[12]:
+
+
+float(180)
+
+
+# In[13]:
+
+
+str(180)
+
+
+# In[ ]:
+
+
+
+
